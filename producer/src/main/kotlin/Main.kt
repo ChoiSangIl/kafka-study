4f0dbc3fb6ec1848fb5153bab1b555b3c9c90d06
Choi Sang Il <wo566@naver.com>
2024-04-21 17:08:45 +0900

    feat: kafka producer

--- producer/src/main/kotlin/Main.kt
+++ producer/src/main/kotlin/Main.kt
@@ -1,15 +1,53 @@
 package org.example
 
+import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import java.time.LocalDateTime
 import java.util.*
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+object KafkaProducerProvider{
+    val producer: KafkaProducer<String, String>
+
+    init {
+        val kafkaProps = Properties()
+        kafkaProps["bootstrap.servers"] = "localhost:9092,localhost:9093,localhost:9094"
+        kafkaProps["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        kafkaProps["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+
+        this.producer = KafkaProducer<String, String>(kafkaProps)
+    }
+
+}
+
+object KafkaRecordHelper{
+    fun createOrder(): ProducerRecord<String, String> = ProducerRecord("createdOrder", "orders", "{\"orderId\":\"${Random.nextInt(1..100000)}\",\"createdAt\":\"${LocalDateTime.now()}\"}")
+}
+
+class DemoProducerCallback: Callback{
+    override fun onCompletion(recordMetadata: RecordMetadata, error: Exception?) {
+        println("비동기 콜백 recordMetadata: $recordMetadata")
+        error?.printStackTrace()
+    }
+}
 
 fun main() {
-    val kafkaProps = Properties()
-    kafkaProps["bootstrap.servers"] = "localhost:9092"
-    kafkaProps["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
-    kafkaProps["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+    //동기적 전송
+    try {
+        KafkaProducerProvider.producer.send(KafkaRecordHelper.createOrder()).get()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
-    val producer = KafkaProducer<String, String>(kafkaProps)
+    //비동기 (콜백) 전송
+    try {
+        KafkaProducerProvider.producer.send(KafkaRecordHelper.createOrder(), DemoProducerCallback())
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
-    println("test")
+    Thread.sleep(2000)
 }
